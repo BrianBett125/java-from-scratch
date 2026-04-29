feb60d77a96f71073d0da3eda071caa15a7084fd
BrianBett125 <brianbett756@gmail.com>
2026-04-29 16:08:34 +0300

    learning to make http requests using api

---
+++ api_demo/Main.java
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/users/1"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(
+                request,
+                HttpResponse.BodyHandlers.ofString()
+        );
+
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Response Body:");
+        System.out.println(response.body());
+    }
+}
