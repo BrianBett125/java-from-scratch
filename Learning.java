92a4de7335df20942ce7846c5cc57f88a1edb39b
BrianBett125 <brianbett756@gmail.com>
2026-04-30 23:17:28 +0300

    learning variables in Java

---
+++ Learning.java
@@ -0,0 +1,7 @@
+public class Learning {
+    public static void main(String[] args) {
+        System.out.println("I am learning Java");
+        System.out.println("welcome Brian");
+        System.out.println("This is my first program");
+    }
+}
---
+++ VariableName.java
@@ -0,0 +1,9 @@
+public class VariableName {
+    public static void main(String[] args) {
+        String name = "Brian";
+
+        System.out.println("Welcome " + name);
+        System.out.println(name + " is learning java");
+        System.out.println(name + " will become a backend engineer");
+    }
+}
