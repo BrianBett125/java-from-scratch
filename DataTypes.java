699d055ca732dcd4343618f27daff3dac60f548b
BrianBett125 <brianbett756@gmail.com>
2026-04-30 23:38:43 +0300

    working with data types

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes { //learning about data types
+    public static void main(String[] args) {
+        String name = "Brian";
+        int age = 20;
+        double height = 1.75;
+        boolean isstudent = true;
+
+        System.out.println("Name: " + name); //prints the name
+        System.out.println("Age: " + age); // prints the age
+        System.out.println("Height: " + height); //prints the height
+        System.out.println("Student: " + isstudent);
+    }
+}
