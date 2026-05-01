b06e4ba776889fe9c794a5054ca12ccd29d0190e
BrianBett125 <brianbett756@gmail.com>
2026-05-01 12:32:05 +0300

    Decision making program  using if-else

---
+++ Decision.java
@@ -0,0 +1,11 @@
+public class Decision { 
+    public static void main(String[] args) {
+       int age = 27;
+
+       if(age >= 18) {
+           System.out.println("Access granted");
+       } else { //invoking and else statement in the program
+           System.out.println("Access denied");
+       }
+    }
+}
