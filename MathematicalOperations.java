d7655527051c092365bd30aeb91a1697bd4bbc38
BrianBett125 <brianbett756@gmail.com>
2026-05-01 12:10:33 +0300

    learning mathematical operations

---
+++ MathematicalOperations.java
@@ -0,0 +1,19 @@
+public class MathematicalOperations {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 4;
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+        System.out.println("Is a greater than b?: " + (a > b));
+    }
+}
+
+
+
+
+
