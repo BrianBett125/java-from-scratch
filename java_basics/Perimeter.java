253fa24e7ea3bbe4d48b5fe3e4adf0152d9c5323
BrianBett125 <brianbett756@gmail.com>
2026-05-03 00:10:35 +0300

    updated Perimeter.java

--- java_basics/Perimeter.java
+++ java_basics/Perimeter.java
@@ -1,17 +1,16 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Perimeter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int perimeter
-        
-        perimeter = 2(length + width);
 
         System.out.println("Enter the length:");
         int length = input.nextInt();
 
         System.out.println("Enter the width:");
         int width = input.nextInt();
+
+        int perimeter = 2 * (length + width);
 
         System.out.println("The perimeter is: " + (perimeter));
 
