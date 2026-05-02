fc7b2d5a2d2c09e05944ae4a0597ca687f36a78a
BrianBett125 <brianbett756@gmail.com>
2026-05-02 23:59:46 +0300

    the perimeter of a rectangle

---
+++ java_basics/Perimeter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner
+
+public class Perimeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int perimeter
+        
+        perimeter = 2(length + width);
+
+        System.out.println("Enter the length:");
+        int length = input.nextInt();
+
+        System.out.println("Enter the width:");
+        int width = input.nextInt();
+
+        System.out.println("The perimeter is: " + (perimeter));
+
+        input.close();
+    }
+}
