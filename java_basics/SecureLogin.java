068773ec367897838fd9a7422609a6cbc0959a95
BrianBett125 <brianbett756@gmail.com>
2026-05-02 17:31:53 +0300

    invoking scanner for input and output operations

--- java_basics/SecureLogin.java
+++ java_basics/SecureLogin.java
@@ -1,12 +1,24 @@
+import java.utils.Scanner; 
+
 public class SecureLogin {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String username = "admin";
         String password = "1234";
 
-        if (username.equals("admin") && password.equals("1234")) {
-            System.out.println("Login successful");
-        } else {
+        system.out.println("Enter username");
+        String username = input.nextLine();
+
+        system.out.println("Enter password");
+        String password = input.nextLine();
+
+        if(username.equals("admin") && password.equals("1234")) {
+            system.out.println("Login successful");
+        }
+        else {
             System.out.println("Invalid credentials");
         }
+        input.close();
     }
 }
+
