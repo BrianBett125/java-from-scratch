6c0554376066fe23bcc1b71c9b98046a9b6f6bcc
BrianBett125 <brianbett756@gmail.com>
2026-05-02 17:48:08 +0300

    fixed log in error

--- java_basics/SecureLogin.java
+++ java_basics/SecureLogin.java
@@ -1,19 +1,17 @@
-import java.utils.Scanner; 
+import java.util.Scanner; 
 
 public class SecureLogin {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String username = "admin";
-        String password = "1234";
 
-        system.out.println("Enter username");
+        System.out.println("Enter username: ");
         String username = input.nextLine();
 
-        system.out.println("Enter password");
+        System.out.println("Enter password: ");
         String password = input.nextLine();
 
         if(username.equals("admin") && password.equals("1234")) {
-            system.out.println("Login successful");
+            System.out.println("Login successful");
         }
         else {
             System.out.println("Invalid credentials");
