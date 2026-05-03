eb6b4ffcd1584fbf927334a3dbee298d8723bf00
BrianBett125 <brianbett756@gmail.com>
2026-05-03 21:13:06 +0300

    calculating the areaof a square

---
+++ java_basics/Square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length");
+        int length = input.nextInt();
+
+        System.out.println("Enter the width");
+        int width = input.nextInt();
+
+        int area = length * width;
+
+        System.out.println("The area of the square is: " + (area));
+
+        input.close();
+        }
+}        
